db180aab16d088b743c054297c3424b44c315156
arvinmz19 <arvinmzadeh@gmail.com>
2026-08-04 13:16:25 +0200

    Erweiterung: Zeitstempel wird aus Dateiname eingelesen und in JSON ergänzt, falsche Dateien werden verworfen, alle Dateien außer Example_JSON werden nach Verwertung gelöscht (!), Daten werden an Verwaltung gepostet

--- Bodenstation.py
+++ Bodenstation.py
@@ -1,8 +1,53 @@
 import json
-
+import pathlib
 import requests
+import os
 
 from models import DataModel, UpdateDataModel
+
+def clean_filename(inp): # bereinigt Dateinamen von Dateipfad und -suffix
+    temp_filename = list(inp)[5:-5]
+    temp = ""
+    for i in range(len(temp_filename)):
+        temp += temp_filename[i]
+    return temp
+
+
+def receive_sat_files():
+    data_path = pathlib.Path("data")
+    input_daten = []
+
+    for item in data_path.iterdir(): # alle Daten im Ordner data
+        if item.is_file():
+            if item.suffix == ".json": # Datentyp prüfen
+                with open(item, "r") as f: # öffnen zum Auslesen
+                    json_content = f.readlines()[0]
+                    json_content = list(json_content)
+                    
+                    filename = clean_filename(str(item))
+                    if not json_content[0] == "{": # TODO: bessere Validierung über Dictionary
+                        print("Fehlerhafte Datei gefunden: erstes Zeichen beginnt nicht mit {")
+                    else:
+                        datensatz = '{"time": "' + filename + '", '
+                        for i in range(1, len(json_content)):
+                            datensatz += json_content[i]
+                        input_daten.append(datensatz)
+     
+            else:
+                print("Fehlerhafte Datei gefunden:", str(item.suffix))
+
+        if str(item) != "data\Example_data.json":
+            os.remove(item)
+
+    return input_daten
+
+
+sat_daten = receive_sat_files()
+print(sat_daten)
+
+for i in range(len(sat_daten)):
+    response = requests.post("http://127.0.0.1:8000/data/", sat_daten[i], headers={"Content-Type": "application/json"})
+    print(response, response.content)
 
 # TODO: Einlesen und Formatierung von Dateien aus dem Ordner "data"
 # TODO: Markierung, sodass Dateien nicht doppelt gelesen werden
@@ -12,14 +57,18 @@ from models import DataModel, UpdateDataModel
 # TODO: Wie senden wir Dateien an die Verwaltung?
 # TODO: ...
 
+'''
+
 if __name__ == '__main__':
     data = DataModel(name="Test")
     new_data = UpdateDataModel(name="Updated Test")
-    answer1 = requests.post("http://127.0.0.1:8000/data/", data.json())
-    answer2 = requests.put(f"http://127.0.0.1:8000/data/{json.loads(answer1.content)['_id']}", new_data.json())
+    answer1 = requests.post("http://127.0.0.1:8000/data/", data.json(), headers={"Content-Type": "application/json"})
+    answer2 = requests.put(f"http://127.0.0.1:8000/data/{json.loads(answer1.content)['_id']}", new_data.json(), headers={"Content-Type": "application/json"})
     answer3 = requests.delete(f"http://127.0.0.1:8000/data/{json.loads(answer1.content)['_id']}")
     answer4 = requests.get(f"http://127.0.0.1:8000/data/{json.loads(answer1.content)['_id']}")
     print(answer1.content)
     print(answer2.content)
     print(answer3.content)
     print(answer4.content)
+
+'''
