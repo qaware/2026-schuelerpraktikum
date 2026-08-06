ec6536d91d4d3b70843777bb69f2a79a8920165f
David Rösler <david_r_roesler@icloud.com>
2026-08-06 10:44:48 +0200

    wip

--- webapp/src/SensorTable.jsx
+++ webapp/src/SensorTable.jsx
@@ -4,43 +4,105 @@ import { useEffect, useState } from "react";
 function SensorTable() {
     const [data, setData] = useState(null);
 
+    const [thrusterData, setThrusterData] = useState([]);
+    const [oxygenTankData, setOxygenTankData] = useState([]);
+    const [hydrogenTankData, setHydrogenTankData] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("http://127.0.0.1:8000/data/");
       const json = await response.json();
       console.log(JSON.stringify(json))
       setData(json);
+
+      setThrusterData(json.filter((item) => item.type === "thruster"));
+      setOxygenTankData(json.filter((item) => item.type === "gas_valve" && item.name.startsWith("o")));
+      setHydrogenTankData(json.filter((item) => item.type === "gas_valve" && item.name.startsWith("h")));
     };
     fetchData();
   }, []);
 
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Pressure</th>
-                    <th>Temperature</th>
-                </tr>
-            </thead>
-        <tbody>
-
-            {
-                data && data.map(sensor => (
-
-                    
+        <>
+            <table>
+                <thead>
                     <tr>
-                        <td>{sensor.name}</td>
-                        <td>{sensor.pressure}</td>
-                        <td>{sensor.temperature}</td>
+                        <th>Name</th>
+                        <th>Pressure</th>
+                        <th>Temperature</th>
                     </tr>
-                ))
-            }
+                </thead>
+            <tbody>
 
-        </tbody>
-        </table>
-    );
+                {
+                    thrusterData && thrusterData.map(sensor => (
+
+                        
+                        <tr>
+                            <td>{sensor.name}</td>
+                            <td>{sensor.pressure}</td>
+                            <td>{sensor.temperature}</td>
+                        </tr>
+                    ))
+                }
+
+            </tbody>
+            <h2>Oxygen Tanks</h2>
+            </table>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Pressure</th>
+                        <th>Temperature</th>
+                    </tr>
+                </thead>
+            <tbody>
+
+                {
+                    oxygenTankData && oxygenTankData.map(sensor2 => (
+
+                        
+                        <tr>
+                            <td>{sensor2.name}</td>
+                            <td>{sensor2.pressure}</td>
+                            <td>{sensor2.temperature}</td>
+                        </tr>
+                    ))
+                }
+
+            </tbody>
+            </table>
+            <h2>Hydrogen Tanks</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Pressure</th>
+                        <th>Temperature</th>
+                    </tr>
+                </thead>
+            <tbody>
+
+                {
+                    hydrogenTankData && hydrogenTankData.map(sensor3 => (
+
+                        
+                        <tr>
+                            <td>{sensor3.name}</td>
+                            <td>{sensor3.pressure}</td>
+                            <td>{sensor3.temperature}</td>
+                        </tr>
+                    ))
+                }
+
+            </tbody>
+            </table>
+    </>
+
+        );
 }
 
 export default SensorTable;
